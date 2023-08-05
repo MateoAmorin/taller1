1a729a0c55fccf6e1d7342b709c6e7b4af11ad74
mateoamorin <mateoac2016@gmail.com>
2023-08-05 00:25:20 -0300

    S

--- eventos.js
+++ eventos.js
@@ -5,12 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Hola! Soy el div");
     });
   
-   /* const buttonElement = document.getElementById("saludarBtn");
-    
-    
-    buttonElement.addEventListener("click", function () {
-alert("Hola!");});*/
-   
+
 
  buttonElement.addEventListener("click", function (event) {
         event.stopPropagation(); // Detiene la propagación del evento al div
