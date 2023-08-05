5e83f4b279af02a22c25316eb003e8be2fc7b85d
mateoamorin <mateoac2016@gmail.com>
2023-08-05 00:32:00 -0300

    S

--- eventos.js
+++ eventos.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  
+ 
     const divElement = document.querySelector("div");
-  
-    divElement.addEventListener("click", function () {
-      alert("Hola! Soy el div");
-    });
-  
-
-
- buttonElement.addEventListener("click", function (event) {
+    
+      divElement.addEventListener("click", function (event) {
+        alert("Hola! Soy el div");
         event.stopPropagation(); // Detiene la propagación del evento al div
-            alert("Hola!");
-   
-    });
- });
-  
+      });
+    
+      
+      });
+    const buttonElement = document.getElementById("saludarBtn");
+    
+      buttonElement.addEventListener("click", function (event) {alert("Hola!");
+        event.stopPropagation(); // Detiene la propagación del evento al div
+      });
+    
